964afdde99072ad71c7d7057affe05f165fff4d3
Wytse Vellema <w.vellema@slash2.nl>
2024-12-13 20:06:55 +0100

    Use the correct struct size for winsize syscall

--- terminal_size.go
+++ terminal_size.go
@@ -1,3 +1,4 @@
+//go:build !windows
 // +build !windows
 
 package uilive
@@ -10,8 +11,10 @@ import (
 )
 
 type windowSize struct {
-	rows    uint16
-	cols    uint16
+	rows   uint16
+	cols   uint16
+	xPixel uint16
+	yPixel uint16
 }
 
 var out *os.File
@@ -24,13 +27,15 @@ func getTermSize() (int, int) {
 		if err != nil {
 			return 0, 0
 		}
-
+		defer out.Close()
 	} else {
 		out, err = os.OpenFile("/dev/tty", os.O_WRONLY, 0)
 		if err != nil {
 			return 0, 0
 		}
+		defer out.Close()
 	}
+
 	_, _, _ = syscall.Syscall(syscall.SYS_IOCTL,
 		out.Fd(), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&sz)))
 	return int(sz.cols), int(sz.rows)
